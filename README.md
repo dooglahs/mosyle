b0a894f5c408a4b25baed1d3c9782e3067c22669
Douglas Nerad <dooglahs@users.noreply.github.com>
2020-07-15 12:27:28 -0400

    DEP improvements
    Improvements to the DEP script based on feedback from Adam Codega.

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ I am currently using Mosyle's Business instance, though these should work just a
 ## Scripts
 
 * Default Userspace Settings. Allows you to set default settings for the user where a profile might work, but you want to allow the user to later change the settings. Setting the Desktop Picture is an example.
-* DEP Default Userspace Settings. Same as the above but works via the DEP process with limitations. See the header in the script for more information.
-* ZoomIT Plist Installer. Zoom has an IT installer, and provides an example plist of the settings you can set. This script deploys your customized plist and versions the plist so older versions can be updated over time.
+* DEP Default Userspace Settings. Same as the above but works via the DEP process with limitations. See the header in the script for more information. Some improvements recommended by `adamcodega` on the MacAdmins Slack.
+* ZoomIT Plist Installer. Zoom has an IT installer, and provides an example plist of the settings you can enforce. This script deploys your customized plist and versions the plist so older versions can be updated over time.
 
 ## Tips
 
-None at this time except to join the MacAdmin's Slack channel!
+None at this time except to join the MacAdmins Slack channel!
 
 ## Tricks
 
--- mosyle-DEP-default-userspace-settings.sh
+++ mosyle-DEP-default-userspace-settings.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 # Setting use defaults for users that you do not want to set via a profile.
-# For example, you might want to set the user's desktop picture, but using a profile the user
-# cannot then change the desktop picture.
+# For example, you might want to set the user's desktop picture, but using a profile
+# the user cannot then change the desktop picture.
 
 # This version supports DEP for your first logged in enduser.
 
 # USE:
 # 1. Put all of the user space commands in the "User space settings" area
-# 2. Create a Custom Command in Mosyle and use your edited script, assign to computers, under the
-#	Schedule tab check "Upon Enrollment" and save.
+# 2. Create a Custom Command in Mosyle and use your edited script, assign
+#	appropriately, then under the Schedule tab check "Upon Enrollment" and save.
 
 # LOGIC:
 # Create a LaunchAgent to launch a script on first user login;
-# Script check for a file to exist; if it does not it creates file and applies settings;
-#	if file does exist it exits.
-# On next login the file will exist so the script will exit.
+# Script checks for a flagging file to exist; if file does not it creates it and
+# applies settings; if flagging file does exist the script exits.
+# On each login, if the flagging file exists, the script will exit.
 
 # LIMITATIONS
 # Creates files that persist on the computer. This is not ideal.
 
-##########################################################################################
+######################################################################################
 # Create LaunchAgent to run script below on first login
-##########################################################################################
+######################################################################################
 
 cat > /Library/LaunchAgents/com.company.user_settings.plist <<LAUNCHAGENTS
 <?xml version="1.0" encoding="UTF-8"?>
@@ -32,7 +32,7 @@ cat > /Library/LaunchAgents/com.company.user_settings.plist <<LAUNCHAGENTS
 	<key>Label</key>
 	<string>com.company.user_settings</string>
 	<key>Program</key>
-	<string>/Users/Shared/.company_user_setting.sh</string>
+	<string>/Library/Application Support/company/.company_user_setting.sh</string>
 	<key>RunAtLoad</key>
 	<true/>
 </dict>
@@ -43,40 +43,52 @@ chown root:wheel /Library/LaunchAgents/com.company.user_settings.plist
 
 
 
-##########################################################################################
-# Write the script to disk so the LaunchAgent can run it
-##########################################################################################
+######################################################################################
+# Create working directory at /Library/Application Support
+######################################################################################
 
-cat > /Users/Shared/.company_user_setting.sh <<XSCRIPT
+if [[ ! -d "/Library/Application Support/company" ]]; then
+	mkdir "/Library/Application Support/company"
+	chown root:wheel "/Library/Application Support/company"
+	chmod 755 "/Library/Application Support/company"
+fi
+
+
+######################################################################################
+# Write the script to disk so the LaunchAgent can run it
+######################################################################################
+
+
+cat > "/Library/Application Support/company/.company_user_setting.sh" <<XSCRIPT
 #!/bin/bash
 
-##########################################################################################
-# First Pass at Script; if already done then skip
-##########################################################################################
+######################################################################################
+# Variables and stuff
+######################################################################################
 
-if [[ -e /Users/Shared/.company_user_setting.txt ]]; then
+USER_NAME=\$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print \$3 }' )
+USER_PATH="/Users/\${USER_NAME}"
+
+######################################################################################
+# First Pass at Script; if already done then skip
+######################################################################################
+
+if [[ -e "\${USER_PATH}"/.company_user_setting.txt ]]; then
 	FIRST_PASS_DONE="yes"
 else
-	touch /Users/Shared/.company_user_setting.txt
+	touch "\${USER_PATH}"/.company_user_setting.txt
 	FIRST_PASS_DONE="no"
 fi
 
 if [[ \${FIRST_PASS_DONE} = no ]]; then
 
-##########################################################################################
-# Variables and stuff
-##########################################################################################
-
-USER_NAME=\$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
-USER_PATH="/Users/\${USER_NAME}"
+######################################################################################
+# User space settings
+######################################################################################
 
 # Insure System Preferences is closed so we can write to some of these
 osascript -e 'tell application "System Preferences" to quit'
 killall cfprefsd
-
-##########################################################################################
-# User space settings
-##########################################################################################
 
 # Set the Desktop Picture
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/Galaxy.jpg"'
@@ -84,9 +96,9 @@ osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/L
 # Unhide the various ~/Library folder
 chflags nohidden "\${USER_PATH}"/Library && xattr -d com.apple.FinderInfo "\${USER_PATH}"/Library
 
-##########################################################################################
+######################################################################################
 # Fix permissions and restart apps and services, delete self
-##########################################################################################
+######################################################################################
 
 killall "Finder"
 sleep 2
@@ -94,9 +106,9 @@ killall "SystemUIServer"
 sleep 2
 exit 0
 
-##########################################################################################
+######################################################################################
 # Second (and all future) Pass at Script
-##########################################################################################
+######################################################################################
 
 # This "else" attaches to the "First Pass at Script" section
 else
@@ -105,4 +117,4 @@ else
 	exit 0
 fi
 XSCRIPT
-chmod +x /Users/Shared/.company_user_setting.sh
+chmod +x "/Library/Application Support/company/.company_user_setting.sh
