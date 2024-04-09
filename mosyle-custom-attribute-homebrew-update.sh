06a211552ce8533c19d6b6a594df8f4405bf2e2f
Douglas Nerad <dooglahs@users.noreply.github.com>
2024-04-09 14:22:00 -0700

    Create mosyle-custom-attribute-homebrew-update.sh
    Mosyle custom attribute that determines if Homebrew is installed or not; returns YES if it needs an update else NO.

---
+++ mosyle-custom-attribute-homebrew-update.sh
@@ -0,0 +1,13 @@
+# Custom attribute to find out if Homebrew is installed, and if so YES or NO if it needs to be updated (NO also indicates it is not installed)
+
+# Run the script as the end user
+sudo -u $(stat -f "%Su" /dev/console) /bin/sh <<'END'
+
+# Is Homebrew installed and needing an update?
+LOCAL_BREW_VERSION=$( /usr/local/bin/brew --version | sed -n 1p | awk '{print $2}' )
+ONLINE_BREW_VERSION=$( curl --silent "https://api.github.com/repos/Homebrew/brew/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' )
+if (( $(echo "${ONLINE_BREW_VERSION} ${LOCAL_BREW_VERSION}" | awk '{print ($1 == $2)}') )) || [[ ! -d /usr/local/Homebrew ]]; then
+	echo "NO"
+else
+	echo "YES"
+fi
