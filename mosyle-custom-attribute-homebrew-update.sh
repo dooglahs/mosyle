d4bba9c003a76bd99e1b5169e743144e66897623
Douglas Nerad <dooglahs@users.noreply.github.com>
2024-04-09 15:32:32 -0700

    Update mosyle-custom-attribute-homebrew-update.sh
    Added error correction if Homebrew isn't present

--- mosyle-custom-attribute-homebrew-update.sh
+++ mosyle-custom-attribute-homebrew-update.sh
@@ -4,9 +4,9 @@
 sudo -u $(stat -f "%Su" /dev/console) /bin/sh <<'END'
 
 # Is Homebrew installed and needing an update?
-LOCAL_BREW_VERSION=$( /usr/local/bin/brew --version | sed -n 1p | awk '{print $2}' )
+LOCAL_BREW_VERSION=$( /usr/local/bin/brew --version 2> /dev/null | sed -n 1p | awk '{print $2}' )
 ONLINE_BREW_VERSION=$( curl --silent "https://api.github.com/repos/Homebrew/brew/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' )
-if (( $(echo "${ONLINE_BREW_VERSION} ${LOCAL_BREW_VERSION}" | awk '{print ($1 == $2)}') )) || [[ ! -d /usr/local/Homebrew ]]; then
+if [[ ! -d /usr/local/Homebrew ]] || (( $(echo "${ONLINE_BREW_VERSION} ${LOCAL_BREW_VERSION}" | awk '{print ($1 == $2)}') )); then
 	echo "NO"
 else
 	echo "YES"
